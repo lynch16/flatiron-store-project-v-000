18d39a444634a95c87d03ee9788e0ff3d22816a8
lynch16 <will.lynch91@gmail.com>
2016-11-26 16:35:23 -0500

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,7 @@
 class CartsController < ApplicationController
 
   def checkout
-    @cart = current_user.current_cart
+    @cart = current_cart
     @cart.line_items.each do |line_item|
       item = Item.find(line_item.item_id)
       item.inventory -= line_item.quantity
@@ -12,7 +12,6 @@ class CartsController < ApplicationController
     @cart.status = "submitted"
     @cart.save
     redirect_to cart_path(@cart)
-    #Post to here. check params, edit cart, inventory, set current_user.current_cart to nil. redirect to cart show page
   end
 
 end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,12 +2,12 @@ class LineItemsController < ApplicationController
 
   def create
     @item = Item.find_by(id: params[:item_id])
-    if current_user.current_cart.nil?
+    if current_cart.nil?
       current_user.current_cart = Cart.create
     end
 
-    current_user.current_cart.line_items << current_user.current_cart.add_item(@item.id)
-    current_user.current_cart.save
-    redirect_to cart_path(current_user.current_cart)
+    current_cart.line_items << current_cart.add_item(@item.id)
+    current_cart.save
+    redirect_to cart_path(current_cart)
   end
 end
